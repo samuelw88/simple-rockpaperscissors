0c918284dda5b47ea55767b40c4fd5663f4f5bc7
samuel <129889128+samuelw88@users.noreply.github.com>
2023-04-14 16:32:53 +0100

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,38 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+choices = ["R", "P", "S"]
+
+while True:
+    user_answer = input("Choose Rock[R], Paper[P], Scissors[S] or Quit[Q]: ").upper()
+    if user_answer == "Q":
+        break
+
+    if user_answer not in ["R", "P", "S"]:
+        continue
+        # if input isn't one of those, ends current iteration and moves onto next
+
+    random_num_generator = random.randint(0, 2)
+    # rock - 0, paper - 1, scissors - 2
+    computer_answer = choices[random_num_generator]
+    print("Computer picked", computer_answer)
+
+    if user_answer == "R" and computer_answer == "S":
+        print("You win!")
+        user_wins += 1
+        continue
+    elif user_answer == "P" and computer_answer == "R":
+        print("You win!")
+        user_wins += 1
+        continue
+    elif user_answer == "S" and computer_answer == "P":
+        print("You win!")
+        user_wins += 1
+        continue
+    else:
+        print("You Lost!")
+        computer_wins += 1
+
+print(f"Your wins: {user_wins}\nComputer wins: {computer_wins}\nGoodbye!")
